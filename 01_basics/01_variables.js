17e3e705fa29b2130d5be524a502669e9f5aa0e6
prajwalpcn1236 <131297393+prajwalpcn1236@users.noreply.github.com>
2025-02-24 05:26:55 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const a=1233
+let email="pcnprajwal@gmail.com"
+var passward="12345"
+
+acctype="savings"
+// dont use var coz it has problem in understanding block and functional scope->{}
+//a=2131
+email="sdaffsdfsf"
+passward="86485"
+
+console.table([acctype,email,passward])
