1cf41f8724e32e3b3ea22e799f4e99a1724b74f0
prajwalpcn1236 <131297393+prajwalpcn1236@users.noreply.github.com>
2025-02-24 05:27:58 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name="prajwal"
+let age=22
+let state="LoggedIn"
+
+//datatypes:
+// number : 2 to the power 53
+// bigint : large numbers
+// boolean : true or false
+// string =""
+// null : stand alone value
+// undefined 
+
+console.log(typeof name)
+console.table([name,age,state]);
+
+console.log(typeof null); // this results to be a object 
+console.log(typeof undefined); //this reults to be a seperate datatype as answer will be undefined itself.
+
