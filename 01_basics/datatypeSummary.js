f3b71a62e341ea819aaf3cb3a7a4e44c32911827
prajwalpcn1236 <131297393+prajwalpcn1236@users.noreply.github.com>
2025-02-24 18:50:39 +0000

    Memory allocation for Datatypes

--- 01_basics/datatypeSummary.js
+++ 01_basics/datatypeSummary.js
@@ -40,3 +40,38 @@ const myfunction=function(){
 console.log(typeof myfunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory and its allocation
+
+/*
+in js among the two datatypes 
+->primitive are all stored in "stack".
+whenever the new variable is assigned the previously declared variable its values copy is 
+assigned to the new variable.
+-> Non-primitive are stored in the "heap".
+whenever the new say object is assigned the previously declared object they both will be
+refrencing the same memory space. hence changing one will affect other as well.
+
+*/
+
+// primitive-> stack (copy is assigned)
+let var1= 100
+let var2=var1
+var2=200
+console.log(var1);
+console.log(var2);
+
+// non-primitive -> heap (memory refrence is given)
+
+let obj1={
+    name:"pcn",
+    rollNo:50
+}
+
+let obj2=obj1
+obj2.rollNo=75
+
+console.log(obj2.rollNo);
+console.log(obj1.rollNo);
